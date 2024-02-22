f78c9edac7fb7974666b569abd6b27bebfa29dbb
Fernando Crozetta <fernando@czetta.com>
2024-02-22 23:58:23 +0100

    build for M1
    this is the only one I need for now.

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ mkdir -p bin
 for prog in "${progs[@]}"
 do
     cd $prog
-    cargo build --release
-    cp ./target/release/$prog ../bin/
+    cargo build --release --target aarch64-apple-darwin
+    cp ./target/aarch64-apple-darwin/release/$prog ../bin/
     cd ..
 done
 cd bin
