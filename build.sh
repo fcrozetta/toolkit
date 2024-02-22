d02f1f99ba460ae089fec5f5349765e49e75771f
Fernando Crozetta <fernando@czetta.com>
2024-02-22 23:41:26 +0100

    wip

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 declare -a progs=("fc-json")
-mkdir -p bin
+mkdir -p artifacts
 for prog in "${progs[@]}"
 do
     cd $prog
     cargo build --release
-    cp ./target/release/$prog ../bin/
+    cd ./target/release/$prog
+    zip $prog
+    cd -
+    cp ./target/release/$prog.zip ../artifacts/
     cd ..
 done
